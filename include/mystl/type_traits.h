105babad34741c750c8ba2563546f657fb03d328
wangshunli <1571374817@qq.com>
2025-08-01 17:23:14 +0800

    type_traits

---
+++ include/mystl/type_traits.h
@@ -0,0 +1,29 @@
+#ifndef __MYSTL_TYPE_TRAITS_H__
+#define __MYSTL_TYPE_TRAITS_H__
+
+#include <type_traits>
+namespace mystl {
+// is_implicitly_default_constructible
+// impl: 如果 T 支持隐式构造，T v = {} 合法；反之不合法
+// 1. 假设 T 支持隐式默认构造，那么 decltype(test_implicitly_construction<T>({}) = void
+//  std::void_t<decltype(test_implicitly_construction<T>({})) 类型参数有效，也就是 void
+// 第二个模版被特化成功，里边的 value 是 true
+// 2. T 不支持隐式默认构造， decltype(test_implicitly_construction<T>({}) 类型无效
+// 第二个模版特化匹配不上，被丢弃，SFINAE，实例化第一个模版，vlaue 是 false
+template <class T>
+void test_implicitly_construction(const T&);
+
+template <class T, class = void>
+struct is_implicitly_default_constructible_impl : std::false_type {};
+
+template <class T>
+struct is_implicitly_default_constructible_impl<
+    T, std::void_t<decltype(test_implicitly_construction<T>({}))>>
+    : std::true_type {};
+
+template <class T>
+inline constexpr bool is_implicitly_default_constructible_v =
+    is_implicitly_default_constructible_impl<T>::value;
+}  // namespace mystl
+
+#endif
