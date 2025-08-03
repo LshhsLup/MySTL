839b20fcd8ea4358e1ec0359e2a5b4067c654049
luoshihao <1571374817@qq.com>
2025-08-04 01:06:31 +0800

    add other functions of pair

--- include/mystl/utility.h
+++ include/mystl/utility.h
@@ -45,7 +45,8 @@ struct pair {
 
   // value constructors
   // T1 和 T2 都能拷贝构造这个构造函数才会参与重载决议
-  //
+  // T1 和 T2 都支持隐式拷贝构造 pair 的这个构造函数在支持隐式构造
+  // std::is_convertible<const U1&, U1>::value 值为 true 说明 U1 支持隐式拷贝构造(U(const U&))
   template <
       class U1 = T1, class U2 = T2,
       typename std::enable_if<std::is_copy_constructible<U1>::value &&
@@ -53,7 +54,7 @@ struct pair {
                                   std::is_convertible<const U1&, U1>::value &&
                                   std::is_convertible<const U2&, U2>::value,
                               int>::type = 0>
-  constexpr pair(const T1& x, const T2& y) : T1(x), T2(y) {}
+  constexpr pair(const T1& x, const T2& y) : first(x), second(y) {}
 
   template <class U1 = T1, class U2 = T2,
             typename = typename std::enable_if<
@@ -62,10 +63,187 @@ struct pair {
                     (!std::is_convertible<const U1&, U1>::value ||
                      !std::is_convertible<const U2&, U2>::value),
                 char>::type>
-  explicit constexpr pair(const T1& x, const T2& y) : T1(x), T2(y) {}
+  explicit constexpr pair(const T1& x, const T2& y) : first(x), second(y) {}
 
-  ~pair() noexcept = default;
+  template <class U1, class U2,
+            typename = typename std::enable_if<
+                std::is_constructible<T1, U1>::value &&
+                    std::is_constructible<T2, U2>::value &&
+                    std::is_convertible<U1, T1>::value &&
+                    std::is_convertible<U2, T2>::value,
+                int>::type>
+  constexpr pair(U1&& x, U2&& y)
+      : first(std::forward<U1>(x)), second(std::forward<U2>(y)) {}
+
+  template <class U1, class U2,
+            typename std::enable_if<std::is_constructible<T1, U1>::value &&
+                                        std::is_constructible<T2, U2>::value &&
+                                        (!std::is_convertible<U1, T1>::value ||
+                                         !std::is_convertible<U2, T2>::value),
+                                    char>::type = 0>
+  explicit constexpr pair(U1&& x, U2&& y)
+      : first(std::forward<U1>(x)), second(std::forward<U2>(y)) {}
+
+  template <class U1, class U2,
+            typename = typename std::enable_if<
+                std::is_constructible<T1, const U1&>::value &&
+                    std::is_constructible<T2, const U2&>::value &&
+                    std::is_convertible<const U1&, T1>::value &&
+                    std::is_convertible<const U2&, T2>::value,
+                int>::type>
+  constexpr pair(const pair<U1, U2>& p) : first(p.first), second(p.second) {}
+
+  template <
+      class U1, class U2,
+      typename std::enable_if<std::is_constructible<T1, const U1&>::value &&
+                                  std::is_constructible<T2, const U2&>::value &&
+                                  (!std::is_convertible<const U1&, T1>::value ||
+                                   !std::is_convertible<const U2&, T2>::value),
+                              char>::type = 0>
+  explicit constexpr pair(const pair<U1, U2>& p)
+      : first(p.first), second(p.second) {}
+
+  template <class U1, class U2,
+            typename = typename std::enable_if<
+                std::is_constructible<T1, U1>::value &&
+                    std::is_constructible<T2, U2>::value &&
+                    std::is_convertible<U1, T1>::value &&
+                    std::is_convertible<U2, T2>::value,
+                int>::type>
+  constexpr pair(pair<U1, U2>&& p)
+      : first(std::forward<U1>(p.first)), second(std::forward<U2>(p.second)) {}
+
+  template <class U1, class U2,
+            typename std::enable_if<std::is_constructible<T1, U1>::value &&
+                                        std::is_constructible<T2, U2>::value &&
+                                        (!std::is_convertible<U1, T1>::value ||
+                                         !std::is_convertible<U2, T2>::value),
+                                    char>::type = 0>
+  explicit constexpr pair(pair<U1, U2>&& p)
+      : first(std::forward<U1>(p.first)), second(std::forward<U2>(p.second)) {}
+
+  pair(const pair&) = default;
+  pair(pair&&) = default;
+
+  //   template <
+  //       class U1 = T1, class U2 = T2,
+  //       typename = typename std::enable_if<std::is_copy_assignable<U1>::value &&
+  //                                              std::is_copy_assignable<U2>::value,
+  //                                          int>::type>
+  typename std::enable_if<std::is_copy_assignable<T1>::value &&
+                              std::is_copy_assignable<T2>::value,
+                          pair&>::type
+  operator=(const pair& other) {
+    first = other.first;
+    second = other.second;
+    return *this;
+  }
+
+  template <
+      class U1, class U2,
+      typename std::enable_if<std::is_assignable<T1&, const U1&>::value &&
+                                  std::is_assignable<T2&, const U2&>::value,
+                              int>::type = 0>
+  pair& operator=(const pair<U1, U2>& other) {
+    first = other.first;
+    second = other.second;
+    return *this;
+  }
+
+  template <class U1 = T1, class U2 = T2,
+            typename = typename std::enable_if<
+                std::is_move_assignable<U1>::value &&
+                std::is_move_assignable<U2>::value>::type>
+  pair& operator=(pair&& other) noexcept(
+      std::is_nothrow_move_assignable<U1>::value&&
+          std::is_nothrow_move_assignable<U2>::value) {
+    first = std::forward<first_type>(other.first);
+    second = std::forward<second_type>(other.second);
+    return *this;
+  }
+
+  template <class U1, class U2,
+            typename = typename std::enable_if<
+                std::is_assignable<T1&, U1&&>::value &&
+                std::is_assignable<T2&, U2&&>::value>::type>
+  pair& operator=(pair<U1, U2>&& other) {
+    first = std::forward<U1>(other.first);
+    second = std::forward<U2>(other.second);
+    return *this;
+  }
+
+  void swap(pair& other) {
+    using std::swap;
+    swap(first, other.first);
+    swap(second, other.second);
+  }
+
+  ~pair() = default;
 };
+
+/* 
+ * Given types std::decay<T1>::type as U1 and std::decay<T2>::type as U2, the types V1
+ * and V2 are defined as follows: 
+ * If U1 is std::reference_wrapper<X>, V1 is X&; otherwise V1 is U1.
+ * If U2 is std::reference_wrapper<Y>, V2 is Y&; otherwise V2 is U2.
+ */
+template <class T>
+struct unwrap_reference_wrapper {
+  using type = T;
+};
+
+template <class T>
+struct unwrap_reference_wrapper<std::reference_wrapper<T>> {
+  using type = T&;
+};
+
+template <class T1, class T2>
+pair<typename unwrap_reference_wrapper<typename std::decay<T1>::type>::type,
+     typename unwrap_reference_wrapper<typename std::decay<T2>::type>::type>
+make_pair(T1&& x, T2&& y) {
+  using U1 = typename std::decay<T1>::type;
+  using U2 = typename std::decay<T2>::type;
+  using V1 = typename unwrap_reference_wrapper<U1>::type;
+  using V2 = typename unwrap_reference_wrapper<U2>::type;
+  return pair<U1, U2>(std::forward<T1>(x), std::forward<T2>(y));
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator==(const std::pair<T1, T2>& lhs,
+                          const std::pair<U1, U2>& rhs) {
+  return (lhs.first == rhs.first) && (lhs.second == rhs.second);
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator!=(const std::pair<T1, T2>& lhs,
+                          const std::pair<U1, U2>& rhs) {
+  return !(lhs == rhs);
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator<(const std::pair<T1, T2>& lhs,
+                         const std::pair<U1, U2>& rhs) {
+  return lhs.first < rhs.second ||
+         (!(rhs.first < lhs.first) && (lhs.second < rhs.second));
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator<=(const std::pair<T1, T2>& lhs,
+                          const std::pair<U1, U2>& rhs) {
+  return !(rhs < lhs);
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator>(const std::pair<T1, T2>& lhs,
+                         const std::pair<U1, U2>& rhs) {
+  return rhs < lhs;
+}
+
+template <class T1, class T2, class U1, class U2>
+constexpr bool operator>=(const std::pair<T1, T2>& lhs,
+                          const std::pair<U1, U2>& rhs) {
+  return !(lhs < rhs);
+}
 }  // namespace mystl
 
 #endif
