c21609a95786cd5da525ec3d5f4f3e97edfe569e
luoshihao <1571374817@qq.com>
2025-08-04 01:07:46 +0800

    test the function that added

--- tests/test_pair.cpp
+++ tests/test_pair.cpp
@@ -1,9 +1,10 @@
 #include <gtest/gtest.h>
 #include <string>
+#include <vector>
 #include "mystl/utility.h"
 #include "utils/test_types.h"
 
-class UtilityTest : public ::testing::Test {
+class PairTest : public ::testing::Test {
  protected:
   // 你可以在这里定义所有测试用例都能访问的对象
   // mystl::pair<int, std::string> p1{1, "hello"};
@@ -13,7 +14,7 @@ class UtilityTest : public ::testing::Test {
 using namespace mystl::test;
 // ---- 测试默认构造函数 ----
 // TEST(TestSuiteName, TestName)
-TEST(UtilityTest, DefaultConstruction) {
+TEST(PairTest, DefaultConstruction) {
   // --- 子测试 1: 基本类型和标准库类型 ---
   {
     mystl::pair<int, double> p_pod;
@@ -101,4 +102,64 @@ TEST(UtilityTest, DefaultConstruction) {
         !std::is_default_constructible_v<mystl::pair<ReferenceMember, int>>,
         "pair with ReferenceMember should not be default constructible");
   }
+}
+
+// pair(const T1&, const T2&)
+TEST(PairTest, ValueConstructor) {
+  // test1: 两个支持隐式拷贝构造函数类型
+  {
+    Perfect::counter.reset();
+    static_assert(std::is_convertible<const Perfect&, Perfect>::value,
+                  "Should support implicit copy");
+    Perfect a(10);
+    Perfect b(20);
+    mystl::pair<Perfect, Perfect> p1 = {a, b};
+    EXPECT_EQ(p1.first.value(), 10);
+    EXPECT_EQ(p1.second.value(), 20);
+    EXPECT_EQ(Perfect::counter.copy_constructor, 2);
+  }
+  // test2: 有显示拷贝构造函数类型
+  {
+    ExplicitCopy::counter.reset();
+    Perfect::counter.reset();
+    static_assert(!std::is_convertible<ExplicitCopy, ExplicitCopy>::value,
+                  "ExplicitCpoy does not support implicit copy");
+    ExplicitCopy a(30);
+    ExplicitCopy b(40);
+    Perfect c(79);
+    // mystl::pair<ExplicitCopy, ExplicitCopy> p2 = {a, b}; // 编译失败
+    mystl::pair<ExplicitCopy, ExplicitCopy> p2{a, b};
+    // mystl::pair<Perfect, ExplicitCopy> p3 = {c , b}; // 编译失败
+    mystl::pair<Perfect, ExplicitCopy> p3{c, b};
+    EXPECT_EQ(p2.first.value(), 30);
+    EXPECT_EQ(p2.second.value(), 40);
+    EXPECT_EQ(p3.first.value(), 79);
+    EXPECT_EQ(p3.second.value(), 40);
+    EXPECT_EQ(ExplicitCopy::counter.value_constructor, 2);
+    EXPECT_EQ(ExplicitCopy::counter.copy_constructor, 3);
+    EXPECT_EQ(Perfect::counter.copy_constructor, 1);
+  }
+  // test3: 内置类型
+  {
+    mystl::pair<int, std::string> p4 = {10, "hello, world"};
+    mystl::pair<std::string, double> p5{"hello, world", 100.11};
+    EXPECT_EQ(p4.first, 10);
+    EXPECT_EQ(p4.second, "hello, world");
+    EXPECT_EQ(p5.first, "hello, world");
+    EXPECT_EQ(p5.second, 100.11);
+  }
+  // 应禁用不可拷贝类型的构造函数
+  {
+    static_assert(!std::is_constructible<
+                      mystl::pair<NonMovableNonCopyable, NoDefault>>::value,
+                  "NonCopyable should not be constructible");
+  }
+}
+
+//
+TEST(PairTest, AssignOperator) {
+  mystl::pair<int, std::vector<int>> p{1, {2}}, q{2, {5, 6}};
+  p = q;
+  EXPECT_EQ(p.first, 2);
+  EXPECT_EQ(p.second.back(), 6);
 }
