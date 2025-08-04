6a9ed542f01c9576c2f4d2ec3188ba0cc82df419
luoshihao <1571374817@qq.com>
2025-08-05 00:31:36 +0800

    add test for default constructor of tuple

--- tests/test_tuple.cpp
+++ tests/test_tuple.cpp
@@ -6,4 +6,11 @@
 
 TEST(TupleTest, Constructors) {
   mystl::tuple<int, std::string, std::vector<int>> t1;
+  std::vector<int> v1{};
+  EXPECT_EQ(mystl::get<0>(t1), 0);
+  EXPECT_EQ(mystl::get<1>(t1), "");
+  EXPECT_EQ(mystl::get<2>(t1), v1);
+  EXPECT_EQ(mystl::get<int>(t1), 0);
+  EXPECT_EQ(mystl::get<std::string>(t1), "");
+  EXPECT_EQ(mystl::get<std::vector<int>>(t1), v1);
 }
