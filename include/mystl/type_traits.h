b76645a815a05c21da6efc440b0cb7071e965304
wangshunli <1571374817@qq.com>
2025-08-04 17:48:50 +0800

    add [is_all_true]

--- include/mystl/type_traits.h
+++ include/mystl/type_traits.h
@@ -2,6 +2,7 @@
 #define __MYSTL_TYPE_TRAITS_H__
 
 #include <type_traits>
+
 namespace mystl {
 // is_implicitly_default_constructible
 // impl: 如果 T 支持隐式构造，T v = {} 合法；反之不合法
@@ -25,12 +26,31 @@ template <class T>
 inline constexpr bool is_implicitly_default_constructible_v =
     is_implicitly_default_constructible_impl<T>::value;
 
+// type: nonsuch
 struct nonsuch {
   nonsuch() = delete;
   nonsuch(const nonsuch&) = delete;
   nonsuch& operator=(const nonsuch&) = delete;
   ~nonsuch() = delete;
 };
+
+// is_all_true: 判断一个模板 Trait 对应的类型是否全为 true
+// 主模板
+template <template <class> class Trait, class... Types>
+struct is_all_true;
+
+// 特化，递归终止条件，所有类型都判断完都是正确的
+template <template <class> class Trait>
+struct is_all_true<Trait> : std::true_type {};
+
+// 特化，递归地判断某个类型是否满足 Trait
+template <template <class> class Trait, class Head, class... Tail>
+struct is_all_true<Trait, Head, Tail...>
+    : typename std::conditional<Trait<Head>::value, is_all_true<Trait, Tail...>,
+                                std::false_type>::type {};
+
+template <template <class> class Trait, class... Types>
+inline constexpr bool is_all_true_v = is_all_true<Trait, Types...>::value;
 }  // namespace mystl
 
 #endif
