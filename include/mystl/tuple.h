ea1d0a7ce073ad999d95608a969337628f9e1ca4
luoshihao <1571374817@qq.com>
2025-08-07 23:28:34 +0800

    add constructors but has unknown bugs

--- include/mystl/tuple.h
+++ include/mystl/tuple.h
@@ -10,9 +10,71 @@
 std::tuple<int, int, int> t1;
 
 namespace mystl {
+// 获取 index 对应的类型
+template <std::size_t Index, class... Types>
+struct NthType;
+
+template <class Head, class... Tail>
+struct NthType<0, Head, Tail...> {
+  using type = Head;
+};
+
+template <std::size_t Index, class Head, class... Tail>
+struct NthType<Index, Head, Tail...> {
+  using type = typename NthType<Index - 1, Tail...>::type;
+};
+
+template <std::size_t Index, class... Types>
+using NthType_t = typename NthType<Index, Types...>::type;
+
+namespace detail {
+template <class Tuple, class OtherTuple,
+          size_t Size =
+              std::tuple_size<typename std::decay<Tuple>::type>::value>
+struct is_unambiguous_conversion_impl {
+  static constexpr bool value = true;  // 默认对多元素 tuple 返回 true
+};
+
+template <class Tuple, class OtherTuple>
+struct is_unambiguous_conversion_impl<Tuple, OtherTuple, 1> {
+  using T =
+      typename std::tuple_element<0, typename std::decay<Tuple>::type>::type;
+  using SourceTupleType = typename std::decay<OtherTuple>::type;
+
+  static constexpr bool is_ambiguous = std::is_convertible_v<OtherTuple, T> ||
+                                       std::is_constructible_v<T, OtherTuple>;
+  static constexpr bool is_same_underlying_tuple =
+      std::is_same_v<typename std::decay<Tuple>::type, SourceTupleType>;
+
+  static constexpr bool value = !is_ambiguous && !is_same_underlying_tuple;
+};
+}  // namespace detail
+
+template <class Tuple, class OtherTuple>
+static constexpr bool is_unambiguous_conversion_v =
+    detail::is_unambiguous_conversion_impl<Tuple, OtherTuple>::value;
 // tuple: 变长参数模板，无法在一个类中存储可变数量和类型的成员
 // 设计思路： 继承一个包含所有元素类型的、独立的基类聚合
 // 基类就是一个带索引和类型的节点
+// 前向声明
+template <class... Types>
+class tuple;
+
+template <std::size_t I, class... Types>
+typename std::tuple_element<I, mystl::tuple<Types...>>::type& get(
+    mystl::tuple<Types...>& t) noexcept;
+
+template <std::size_t I, class... Types>
+typename std::tuple_element<I, mystl::tuple<Types...>>::type&& get(
+    mystl::tuple<Types...>&& t) noexcept;
+
+template <std::size_t I, class... Types>
+const typename std::tuple_element<I, mystl::tuple<Types...>>::type& get(
+    const mystl::tuple<Types...>& t) noexcept;
+
+template <std::size_t I, class... Types>
+const typename std::tuple_element<I, mystl::tuple<Types...>>::type&& get(
+    const mystl::tuple<Types...>&& t) noexcept;
 
 template <std::size_t I, class T>
 struct TupleLeaf {
@@ -66,45 +128,71 @@ class tuple
   template <
       class... UTypes,
       typename std::enable_if<
-          sizeof...(Types) == sizeof...(UTypes) && sizeof...(UTypes) >= 1 &&
+          // 检查参数数量是否匹配
+          sizeof...(UTypes) == sizeof...(Types) &&
+              // 检查是否每个元素都可以从对应的参数构造
               mystl::is_all_true_general_v<std::is_constructible,
                                            mystl::TypeLists<Types...>,
-                                           mystl::TypeLists<UTypes...>>,
+                                           mystl::TypeLists<UTypes...>> &&
+              // 防止此构造函数被误用于拷贝/移动 tuple 本身
+              (sizeof...(UTypes) != 1 ||
+               !std::is_same<tuple<Types...>, typename std::decay<NthType_t<
+                                                  0, UTypes...>>::type>::value),
           int>::type = 0>
   constexpr tuple(UTypes&&... args) : Base(std::forward<UTypes>(args)...) {}
 
-  template <class... UTypes>
-  constexpr tuple(const tuple<UTypes...>& other);
+  template <class... UTypes,
+            typename std::enable_if<
+                sizeof...(UTypes) == sizeof...(Types) &&
+                    mystl::is_all_true_general_v<
+                        std::is_constructible, mystl::TypeLists<Types...>,
+                        mystl::TypeLists<const UTypes&...>> &&
+                    is_unambiguous_conversion_v<mystl::tuple<Types...>,
+                                                const mystl::tuple<UTypes...>&>,
+                int>::type = 0>
+  constexpr tuple(const tuple<UTypes...>& other)
+      : tuple(other, std::make_index_sequence<sizeof...(UTypes)>{}) {}
 
-  template <class... UTypes>
-  constexpr tuple(tuple<UTypes...>&& other);
+  template <class... UTypes,
+            typename std::enable_if<
+                sizeof...(UTypes) == sizeof...(Types) &&
+                    mystl::is_all_true_general_v<
+                        std::is_constructible, mystl::TypeLists<Types...>,
+                        mystl::TypeLists<UTypes&&...>> &&
+                    is_unambiguous_conversion_v<mystl::tuple<Types...>,
+                                                mystl::tuple<UTypes...>&&>,
+                int>::type = 0>
+  constexpr tuple(tuple<UTypes...>&& other)
+      : tuple(std::move(other), std::make_index_sequence<sizeof...(UTypes)>{}) {
+  }
 
-  template <class U1, class U2>
-  constexpr tuple(const mystl::pair<U1, U2>& p);
+  template <class U1, class U2,
+            typename std::enable_if<
+                sizeof...(Types) == 2 &&
+                    mystl::is_all_true_general_v<
+                        std::is_constructible, mystl::TypeLists<Types...>,
+                        mystl::TypeLists<const U1&, const U2&>>,
+                int>::type = 0>
+  constexpr tuple(const mystl::pair<U1, U2>& p) : Base(p.first, p.second) {}
 
-  template <class U1, class U2>
-  constexpr tuple(std::pair<U1, U2>&& p);
+  template <class U1, class U2,
+            typename std::enable_if<
+                sizeof...(Types) == 2 &&
+                    mystl::is_all_true_general_v<std::is_constructible,
+                                                 mystl::TypeLists<Types...>,
+                                                 mystl::TypeLists<U1&&, U2&&>>,
+                int>::type = 0>
+  constexpr tuple(std::pair<U1, U2>&& p)
+      : Base(std::move(p.first), std::move(p.second)) {}
 
   tuple(const tuple& other) = default;
   tuple(tuple&& other) = default;
+
+ private:
+  template <class OtherTuple, std::size_t... Is>
+  constexpr tuple(OtherTuple&& other, std::index_sequence<Is...>)
+      : Base(mystl::get<Is>(std::forward<OtherTuple>(other))...) {}
 };
-
-// 获取 index 对应的类型
-template <std::size_t Index, class... Types>
-struct NthType;
-
-template <class Head, class... Tail>
-struct NthType<0, Head, Tail...> {
-  using type = Head;
-};
-
-template <std::size_t Index, class Head, class... Tail>
-struct NthType<Index, Head, Tail...> {
-  using type = typename NthType<Index - 1, Tail...>::type;
-};
-
-template <std::size_t Index, class... Types>
-using NthType_t = typename NthType<Index, Types...>::type;
 
 // 根据类型获取 Types 中对应的索引
 template <class T, class... Types>
