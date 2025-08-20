06d3661ec78980d7442f622383fb78212928cff2
wangshunli <1571374817@qq.com>
2025-08-20 11:42:41 +0800

    [Feature](vector):add member type

--- include/mystl/vector.h
+++ include/mystl/vector.h
@@ -2,10 +2,32 @@
 #define __MYSTL_VECTOR_H__
 
 #include <memory>
+#include "mystl/allocator.h"
 
 namespace mystl {
-template <class T, class Alloc = std::allocator<T>>
-class vector {};
+
+template <class T, class Alloc = mystl::allocator<T>>
+class vector {
+ public:
+  // member type
+  using value_type = T;
+  using allocator_type = Alloc;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using reference = value_type&;
+  using const_reference = const value_type&;
+  using pointer = typename std::allocator_traits<Alloc>::pointer;
+  using const_pointer = typename std::allocator_traits<Alloc>::pointer;
+  using iterator = T*;
+  using const_iterator = const T*;
+  using revrese_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+ private:
+  pointer start;
+  pointer finish;
+  pointer end_of_storage;
+};
 }  // namespace mystl
 
 #endif
